8489fa0e2b99ea9bdad25559ee27f377f0c6cc58
Vadim Zhukov <persgray@gmail.com>
2021-05-26 15:41:34 +0300

    rework targets (again)
    * Added 'configure' target.
    
    * The unpack target was renamed to extract.
    
    * Cleaned up recursive vs. non-recursive targets.
    
    * Added re* targets, making fetch, patch etc. safe to be called twice.
      Note: unpatch target doesn't have any checks and runs unconditionally.
    
    * There is almost no target code in OS-specific .mk file now.

--- OpenBSD/OpenBSD.mk
+++ OpenBSD/OpenBSD.mk
@@ -16,43 +16,24 @@ COMPONENTS =	kernel	sys.tar.gz
 COMPONENTS +=	base	src.tar.gz
 
 
+.PHONY: install-kernel-headers
+
 ###################
 # Kernel specifics
 
-PATCH_INDICATOR_kernel =	fgrep -w cwfs ${SRC_DIR_kernel}/conf/files
-KERNEL_CONFIG_INDICATOR =	${OBJ_DIR_kernel}/Makefile ${OBJ_DIR_kernel}/options
+UNPACK_kernel_PREFIX = sys
+COOKIE_configure_kernel = ${OBJ_DIR_kernel}/Makefile ${OBJ_DIR_kernel}/options
 KERNEL_CONF_DIR =	${SRC_DIR_kernel}/arch/${OS_PLAT_CPU}/conf
-KERNEL_HEADERS !=	grep ^'Index: sys/.*\.h' ${PATCH_DIR}/cw-kernel.patch | \
+KERNEL_HEADERS !=	grep '^Index: sys/.*\.h' ${PATCH_DIR}/cw-kernel.patch | \
 			cut -d ' ' -f 2
 
-do-unpack-kernel:
-	mkdir -p ${SRC_DIR_kernel}
-	${GENERATE_UNPACK_CAT_CMD}; \
-	cd ${SRC_DIR_kernel}/..; \
-	unpack_cat ${FETCH_DIR}/sys.tar.gz | \
-	    pax -r -s ',^\(./\)*sys,${SRC_DIR_kernel:C,.*/,,},'
-	mkdir -p ${SRC_DIR_kernel}/cwfs
-
-do-build-kernel: ${KERNEL_CONFIG_INDICATOR}
-	cd ${OBJ_DIR_kernel}; ${MAKE_CMD}
-
-${KERNEL_CONFIG_INDICATOR} kernel-config:
+do-configure-kernel:
 	cd ${KERNEL_CONF_DIR}; \
 	mkdir -p ${OBJ_DIR_kernel}; \
 	config -b ${OBJ_DIR_kernel} -s ${SRC_DIR_kernel} ${KERNEL_CONFIG}
 
-install-kernel:
-	cd ${OBJ_DIR_kernel}; ${DOAS} ${MAKE_CMD} install
-
-clean-kernel:
-	cd ${OBJ_DIR_kernel} && ${MAKE_CMD} clean || true
-
-distclean-kernel:
-	rm -f ${KERNEL_CONFIG_INDICATOR}
-	rm -Rf ${OBJ_DIR_kernel} ${SRC_DIR_kernel}
-
 .for _h in ${KERNEL_HEADERS}
-kernel-headers: /usr/include/${_h}
+install-kernel-headers: /usr/include/${_h}
 /usr/include/${_h}: ${SRC_DIR_kernel}/${_h}
 	${DOAS} install -o root -g bin -m 0444 $> $@
 .endfor
@@ -61,42 +42,28 @@ kernel-headers: /usr/include/${_h}
 ####################
 # Base OS specifics
 
-PATCH_INDICATOR_base =		fgrep -w mount_cwfs ${SRC_DIR_base}/sbin/Makefile
+BUILD_DIR_base =	${SRC_DIR_base}
 
-BASE_DIRS = \
+BUILD_SUBDIRS_base = \
 	sbin/mount \
-	sbin/mount_cwfs \
-	share/man
+	sbin/mount_cwfs
 
-BASE_INSTALL_EXTRA = \
+BASE_MAN_PAGES != grep '^Index: .*\.[0-9]$$' ${PATCH_DIR}/cw-base.patch | \
+		  cut -d ' ' -f 2
+
+INSTALL_EXTRA_base = \
 	etc/etc.${OS_PLAT}/MAKEDEV	root	wheel	0555	/dev \
 	lib/libc/sys/mount.2		root	bin	0444	/usr/share/man/man2
+.for _man in ${BASE_MAN_PAGES}
+INSTALL_EXTRA_base +=	${_man}		root	bin	0444	/usr/${_man}
+.endfor
 
-do-unpack-base:
-	mkdir -p ${SRC_DIR_base}
-	${GENERATE_UNPACK_CAT_CMD}; \
-	cd ${SRC_DIR_base}; \
-	unpack_cat ${FETCH_DIR}/src.tar.gz | pax -r
-	mkdir -p ${SRC_DIR_base}/sbin/mount_cwfs
-
-# ignores e.g. sbin/mount_cwfs/ in unpatched mode
-do-build-base: kernel-headers
-	mkdir -p ${OBJ_DIR_base}
-.for _d in ${BASE_DIRS}
-. if exists(${SRC_DIR_base}/${_d}/Makefile)
-	cd ${SRC_DIR_base}/${_d}; ${MAKE_CMD} obj all
+do-configure-base:
+.for _d in ${BUILD_SUBDIRS_base}
+. if exists(${BUILD_DIR_base}/${_d}/Makefile)
+	cd ${BUILD_DIR_base}/${_d}; make obj
 . endif
 .endfor
 
-install-base:
-.for _d in ${BASE_DIRS}
-	cd ${SRC_DIR_base}/${_d}; ${DOAS} ${MAKE_CMD} install
-.endfor
-.for _f _o _g _m _d in ${BASE_INSTALL_EXTRA}
-	cd ${SRC_DIR_base}; ${DOAS} install -c -o ${_o} -g ${_g} -m ${_m} ${_f} ${DESTDIR}${_d}
-.endfor
 
-clean-base:
-.for _d in ${BASE_DIRS}
-	cd ${SRC_DIR_base}/${_d} && ${MAKE_CMD} clean || true
-.endfor
+build-base: ${KERNEL_HEADERS:C,^,/usr/include/,}
